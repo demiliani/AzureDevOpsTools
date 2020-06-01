22006049fca3eff3d6b7efa56b9b1f2c28bc16bc
Stefano Demiliani <33155438+demiliani@users.noreply.github.com>
2020-06-01 11:20:05 +0200

    Create Work Item script (Powershell)

---
+++ Scripts/CreateWorkItem.ps1
@@ -0,0 +1,31 @@
+﻿#Authentication in Azure DevOps
+$AzureDevOpsPAT = 'YOUR_PERSONAL_ACCESS_TOKEN'
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($AzureDevOpsPAT)")) }
+
+$OrganizationName = "YOUR_AZURE_DEVOPS_ORGANIZATION_NAME"
+$UriOrganization = "https://dev.azure.com/$($OrganizationName)/"
+
+#Lists all projects in your organization
+$uriAccount = $UriOrganization + "_apis/projects?api-version=5.1"
+Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicationHeader 
+
+
+#Create a work item
+
+$WorkItemType = "task"
+$WorkItemTitle = "Test from Powershell"
+$ProjectName = "YOUR_PROJECT_NAME";
+
+
+$uri = $UriOrganization + $ProjectName + "/_apis/wit/workitems/$" + $WorkItemType + "?api-version=5.1"
+echo $uri
+
+$body="[
+  {
+    `"op`": `"add`",
+    `"path`": `"/fields/System.Title`",
+    `"value`": `"$($WorkItemTitle)`"
+  }
+]"
+
+Invoke-RestMethod -Uri $uri -Method POST -Headers $AzureDevOpsAuthenicationHeader -ContentType "application/json-patch+json" -Body $body
